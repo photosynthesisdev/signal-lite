4a2decf8b0f203bc8c3ab8dcf2b3521e4d2c66d6
Danny O <email@email.com>
2024-09-11 19:34:22 +0000

    Commented and Documented

--- backend/api.py
+++ backend/api.py
@@ -27,47 +27,35 @@ class MessageRecord(BaseModel):
 @router.websocket("/chatConnect")
 async def chatConnect(websocket: WebSocket):
     async def send_message_to_client(message_record: str):
-        # Basically ignore this. All you need to know is that it sends a messasge record to  our client.
-        try:
-            await websocket.send_text(message_record)
-        except Exception as e:
-            logging.error(f"Error sending message to client: {e}")
+        # sends message record to a client.
+        await websocket.send_text(message_record)
     def watch_callback(watch_response):
         for event in watch_response.events:
             if isinstance(event, etcd3.events.PutEvent):
                 # 'value' is the MessageRecord that was just written to the database.
                 value = event.value.decode("utf-8")
-                # Ignore this largely, not relevant to discussion of websockets. 
-                # In short, it's just a thread safe way to send message to clients b/c 'watch_callback' is a synchronous function.
+                # In short, this is just a thread safe way to send message to clients b/c 'watch_callback' is a synchronous function.
                 asyncio.run_coroutine_threadsafe(send_message_to_client(value), loop)
-    
     # --- ACCEPT ANY INCOMING CONNECTION ---
     # If you view 'frontend/main.js', you can see that as soon as the site loads for the client, they establish a connection to "wss://signallite.io/api/chatConnect"
     await websocket.accept()
-
     # ---- CREATE & SEND USER ID -----
     # The server selects a random user_id for the client to identify themselves as.
     # This ID does not persist between sessions, so every time a client refreshes the page (and connects to this websocket) they will receive a new user_id.
     user_id = random.randint(0, 1000000)
     local_client_json_string = LocalClient(local_client_user_id = user_id).json()
     await websocket.send_text(local_client_json_string)
-
-
     # ---- SEND OLD CHAT MESSAGE RECORDS TO CLIENT -----
     # This goes to the database key "/v1/messages" and gets all old messages sent in the chat before this user came to the website.
     old_message_data = database.get_prefix("/v1/messages")
     for old_message in old_message_data:
         msg = old_message[0].decode('utf-8')
         await websocket.send_text(msg)
-
-    
     # ---- LISTEN FOR NEW MESSAGES -----
     # Over the lifespan of this connection, we want to ensure any new messages sent by clients are broadcasted to all other clients.
     # We do this via the notion of a 'watch' which looks at a key prefix, and sends updates.
     loop = asyncio.get_event_loop()
     watch_id = database.add_watch_prefix_callback("/v1/messages", watch_callback)
-
-
     # -------- WHILE TRUE RECEIVE LOOP --------
     # This keeps the websocket connection alive for the duration of the clients connection.
     # When the client disconnects (closes the tab), a WebSocket exception will be raised, hence why we have the try/except block.
@@ -78,11 +66,11 @@ async def chatConnect(websocket: WebSocket):
             # Once the client sends a message, create a MessageRecord object, and store it in the database.
             message_record = MessageRecord(
                 user_id = user_id, 
-                username = username, 
                 timestamp = time.time(), 
                 message = message_from_client
             )
             # Once we store this in the database, it will notify all users connected to the websocket that a new piece of message was sent to the database.
+            # It notifies all other connections because we have a 'watch' on the key "/v1/messages".
             database.put(f"/v1/messages/{time.time()}", value = message_record.json())
     except Exception as e:
         try:
